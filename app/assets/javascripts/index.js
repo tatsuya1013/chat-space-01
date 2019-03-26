a740f151dfd04af8cde337ef187b5a73fc2695ad
tatsuya1013 <tatsuya101344@icloud.com>
2019-03-26 13:09:20 +0900

    Add

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,6 @@
+$(function() {
+  $("#user-search-field.chat-group-form__input").on("keyup" , function() {
+    var input = $("#user-search-field.chat-group-form__input").val();
+    console.log(input);
+  });
+});
